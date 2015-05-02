f10c4df39cbbaf6a117e7d0a8d5eebdd772ffa43
Sho Yokoyama <sho.y0921@gmail.com>
2015-05-02 10:26:14 +0900

    crawl and scrape apple-mac-book page

--- lib/crawls/get_device_mac.rb
+++ lib/crawls/get_device_mac.rb
@@ -1,35 +1,44 @@
+# encoding: utf-8
+
 class Crawls::GetDeviceMac
+	# rails runner lib/crawls/get_device_mac.rb
+
+	require 'open-uri'
+	require 'kconv'
+	require 'nokogiri'
 	
-	# Property:ItemURL
-	@mac_book_url = 'http://www.apple.com/jp/macbook/specs/'
 	@mac_book_pro_url = 'http://www.apple.com/jp/macbook-pro/specs/'
 	@mac_book_pro_retina_url = 'http://www.apple.com/jp/macbook-pro/specs-retina/'
 	@mac_book_air_url = 'http://www.apple.com/jp/macbook-air/specs.html'
 
 	# Model
-	brand = DeviceBrand.new(:name => 'Mac')
-	brand.tap(&:save)
+	#brand = DeviceBrand.new(:name => 'Mac')
+	#brand.tap(&:save)
 
 
 
-	# MacBook
+	# MacBook 
+	mac_book_doc = Nokogiri::HTML(open("http://www.apple.com/jp/macbook/specs/", &:read).toutf8)
+	mac_book_size_doc = mac_book_doc.at("//*[@id=\"main\"]/section[3]/section[6]/div/div[2]")
 
-	# crawl
-	# name
-	mac_book_name = ''
-	# width
-	mac_book_width = 0
-	# height
-	mac_book_height = 0
-	# depth
-	mac_book_depth = 0
-	# insert
+	mac_book_name = mac_book_doc.at("/html/body/div[1]/nav/h2/a").text
+	mac_book_width = mac_book_size_doc.to_s.match(/幅/).post_match.match(/\d+/)[0]
+	mac_book_height = mac_book_size_doc.to_s.match(/高/).post_match.match(/～\d+/)[0].to_s[1..-1]
+	mac_book_depth = mac_book_size_doc.to_s.match(/奥/).post_match.match(/\d+/)[0]
+
 	mac_book = DeviceItem.new(:name => mac_book_name,
-														:width => mac_book_width,
-														:height => mac_book_height,
-														:depth => mac_book_depth,
-														:device_brand_id => brand.id)
-	mac_book.save
+														:width => mac_book_width.to_i + 1,
+														:height => mac_book_height.to_i + 1,
+														:depth => mac_book_depth.to_i + 1,
+														:device_brand_id => 1)
+	#													:device_brand_id => brand.id)
+	#mac_book.save
+
+	puts "name: "   + mac_book.name
+	puts "width: "  + mac_book.width.to_s
+	puts "height: " + mac_book.height.to_s
+	puts "depth: "  + mac_book.depth.to_s
+	puts "brand: "  + mac_book.device_brand_id.to_s
 
 
 
