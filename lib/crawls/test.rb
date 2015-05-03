6a7ad633f3e99233b19ec8df755c8dc77e54e5e1
Sho Yokoyama <sho.y0921@gmail.com>
2015-05-03 15:52:42 +0900

    try to check which script runner read

--- lib/crawls/test.rb
+++ lib/crawls/test.rb
@@ -1,13 +1,40 @@
 # rails runner Crawls::Test.execute
-# => both messages displayed
-class Crawls::Test
-	def self.execute
-    puts "***** Success! *****\n***** Your Rails Runner and Your Job Script did! *****"
-	end
-end
+# 3 => 2 => 5 => 4 => 7 => 1
+
+# rails runner Crawls::Test
+# 3 => 2 => 5 => 4 => 7
 
 # rails runner lib/crawls/test.rb
-# => this messsage displayed
-puts "***** Success! *****\n***** Your Rails Runner and Your Job Script did! *****"
+# 3 => 2 => 5 => 4 => 7 
+
+class Crawls::Test
+	def self.execute
+    puts "***** 1 *****\n***** Success! *****\n***** Your Rails Runner and Your Job Script did! *****"
+	end
+
+	puts "***** 3 *****\n***** Success! *****\n***** Your Rails Runner and Your Job Script did! *****"
+end
+
+puts "***** 2 *****\n***** Success! *****\n***** Your Rails Runner and Your Job Script did! *****"
+
+class Crawls::Try
+	def self.execute
+    puts "***** 4 *****\n***** Success! *****\n***** Your Rails Runner and Your Job Script did! *****"
+    self.try
+    # challenge => Exception
+	end
+
+	def self.try
+    puts "***** 7 *****\n***** Success! *****\n***** Your Rails Runner and Your Job Script did! *****"
+	end
+
+	def challenge
+    puts "***** 6 *****\n***** Success! *****\n***** Your Rails Runner and Your Job Script did! *****"
+	end
+
+	puts "***** 5 *****\n***** Success! *****\n***** Your Rails Runner and Your Job Script did! *****"
+	self.execute
+end
+
 
 
