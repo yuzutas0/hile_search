87871054fd8c49605af7a079d0da3ac6115aa655
Sho Yokoyama <sho.y0921@gmail.com>
2015-05-05 17:19:50 +0900

    make return-policy for crawl amazon-bag-detail

--- lib/crawls/get_bag_detail.rb
+++ lib/crawls/get_bag_detail.rb
@@ -9,31 +9,47 @@ class Crawls::GetBagDetail
 	def self.execute
 		# input => detail-manager
 		# output => save
+		# CrawlBagDetailManager
 	end
 
 
 
-	def self.detail_to_array(detail_url)
-		this_detail = Nokogiri::HTML(open(detail_url, &:read).toutf8)
-		sleep(1)
+	def self.detail_to_array(manager)
 
-		# get parameters
-		detail_url_dp = self.get_url_dp(this_detail)
+		# same check
+		detail_url_dp = manager.url.match(/\/dp\//).post_match
+		if self.check_same_dp_url(detail_url_dp, manager.bag_tag)
+			manager.done_flag = true
+			return
+		end
+
+		# crawl
+		sleep(1)
+		this_detail = Nokogiri::HTML(open(manager.url, &:read).toutf8)
+
+		# get basic parameters
 		detail_name = self.get_name(this_detail)
 		detail_image = self.get_image(this_detail)
 		detail_price = self.get_price(this_detail)
 
+		# get size parameters
 		size_hash = self.get_size(this_detail)
 		detail_width = size_hash["width"]
 		detail_height = size_hash["height"]
 		detail_depth = size_hash["depth"]
 
 		# error check
-		# name, image => nil
-		# price, width, height => 0
-		# depth => 0 (error but save)
+		if detail_name == nil || detail_image == nil || detail_price == 0 || detail_width == 0 || detail_height == 0
+			manager.error_flag = true
+			return
+		end
 
-		# object
+		if detail_depth == 0
+			manager.error_flag = true
+			# keep this script (not return)
+		end
+
+		# save
 		detail_item = BagItem.new(:name      => detail_name,
 															:url_dp    => detail_url_dp,
 															:width     => detail_width,
@@ -41,16 +57,23 @@ class Crawls::GetBagDetail
 															:depth     => detail_depth,
 															:price     => detail_price,
 															:image_url => detail_image)
-		# item * tag (detail_item.tag.push("this tag"))
-		# array.push(detail_item)
+		detail_item.bag_tag << manager.bag_tag
+		detail_item.save
+		manager.done_flag = true
+		return
 	end
 
 
 
-	def self.get_url_dp(this_detail)
-		detail_url_dp = detail_url.match(/\/dp\//).post_match
-		# 既存チェック => tag追加
-		return detail_url_dp
+	def self.check_same_dp_url(detail_url_dp, bag_tag)
+		item = BagItem.find_by(url_dp: detail_url_dp)
+		if item != nil
+			item.bag_tag << bag_tag
+			item.save
+			return true
+		else
+			return false
+		end
 	end
 
 
@@ -157,7 +180,6 @@ class Crawls::GetBagDetail
 				detail_width_string = detail_size_doc.to_s.match(/#{slicer}/).pre_match.match(/[0-9０-９]+/)
 			end
 			detail_width = self.get_size_score_by_slicer(detail_width_string)
-
 			detail_height_string = detail_size_doc.to_s.match(/#{slicer}/).post_match.match(/[0-9０-９]+/)
 			detail_height = self.get_size_score_by_slicer(detail_height_string)
 			
