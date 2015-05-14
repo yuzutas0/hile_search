d9a49f2466098eba16e005cde301496275d727ce
Sho Yokoyama <sho.y0921@gmail.com>
2015-05-14 21:15:15 +0900

    design workflow of searching bag and device size

---
+++ lib/crawls/sort_bag_and_device_size.rb
@@ -0,0 +1,41 @@
+# encoding: utf-8
+class Crawls::SortBagAndDeviceSize
+
+	# rails runner Crawls::SortBagAndDeviceSize.execute
+	def self.execute
+		# puts job start
+		# sort device size
+		# sort bag size
+		# puts job end
+	end
+
+
+	# sort device size
+	# input:device's H,W,D => output:device's LongSide, MiddleSide, ShortSide
+
+	# get all recoad
+	# puts device start
+	# loop
+	# puts start
+	# sort 3 size
+	# add 3 side
+	# loop-end
+	# bulk insert
+
+
+
+	# sort bag size
+	# input:bag's H,W,D => output:bag's LongSide, MiddleSide, ShortSide
+
+	# get all recoad
+	# puts bag start
+	# loop
+	# puts start
+	# sort 3 size
+	# add 3 side
+	# loop-end
+	# bulk update
+
+
+
+end
