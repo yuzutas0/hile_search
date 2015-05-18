73814d440d69f4caad0415e9aec7b874cc691584
Sho Yokoyama <sho.y0921@gmail.com>
2015-05-18 20:53:55 +0900

    make batch for sort bag and device size

--- lib/crawls/sort_bag_and_device_size.rb
+++ lib/crawls/sort_bag_and_device_size.rb
@@ -3,39 +3,85 @@ class Crawls::SortBagAndDeviceSize
 
 	# rails runner Crawls::SortBagAndDeviceSize.execute
 	def self.execute
-		# puts job start
-		# sort device size
-		# sort bag size
-		# puts job end
+		puts "*** job starts!! ***"
+		self.sort_device_size
+		self.sort_bag_size
+		puts "*** job ends!! ***"
 	end
 
 
-	# sort device size
+
 	# input:device's H,W,D => output:device's LongSide, MiddleSide, ShortSide
+	def self.sort_device_size
+		puts "* device sort starts!! *"
+		count = 0
 
-	# get all recoad
-	# puts device start
-	# loop
-	# puts start
-	# sort 3 size
-	# add 3 side
-	# loop-end
-	# bulk insert
+		DeviceItem.find_each do |device|
+			puts "device - loop - " + count.to_s
+			self.sort_each_device_size(device)
+			puts "   updated!"
+			count += 1
+		end
+
+		puts "* device sort ends!! *"
+	end
 
 
 
-	# sort bag size
+	# sort
+	def self.sort_each_device_size(device)
+		size = [0, 0, 0]
+		size[0] = device.height
+		size[1] = device.width
+		size[2] = device.depth
+		size.sort!
+
+		device.short_side = size[0]
+		device.middle_side = size[1]
+		device.long_side = size[2]
+
+		puts "     short_side  = " + device.short_side.to_s
+		puts "     middle_side = " + device.middle_side.to_s
+		puts "     long_side   = " + device.long_side.to_s
+
+		device.save!
+	end
+
+
+
 	# input:bag's H,W,D => output:bag's LongSide, MiddleSide, ShortSide
+	def self.sort_bag_size
+		puts "* bag sort starts!! *"
+		count = 0
 
-	# get all recoad
-	# puts bag start
-	# loop
-	# puts start
-	# sort 3 size
-	# add 3 side
-	# loop-end
-	# bulk update
+		BagItem.find_each do |bag|
+			puts "bag - loop - " + count.to_s
+			self.sort_each_bag_size(bag)
+			puts "   updated!"
+			count += 1
+		end
+
+		puts "* bag sort ends!! *"
+	end
 
 
 
+	# sort
+	def self.sort_each_bag_size(bag)
+		size = [0, 0, 0]
+		size[0] = bag.height
+		size[1] = bag.width
+		size[2] = bag.depth
+		size.sort!
+
+		bag.short_side = size[0]
+		bag.middle_side = size[1]
+		bag.long_side = size[2]
+
+		puts "     short_side  = " + bag.short_side.to_s
+		puts "     middle_side = " + bag.middle_side.to_s
+		puts "     long_side   = " + bag.long_side.to_s
+
+		bag.save!
+	end
 end
