7e5a727276bb7b67dbcf900ea6d98678efd49728
Sho Yokoyama <sho.y0921@gmail.com>
2015-05-18 22:45:49 +0900

    make query for search bag by device

--- app/controllers/bag_items_controller.rb
+++ app/controllers/bag_items_controller.rb
@@ -1,10 +1,25 @@
 class BagItemsController < ApplicationController
 
 	def index
+		@device = DeviceItem.find(params[:id])
+
+		@bags = BagItem.where("long_size > :long_size AND middle_size > :middle_size AND \
+											(short_size = 0 OR short_size > :short_size) AND bag_tag_id IN (:tags)", { 
+													long_size: @device.long_size, 
+													middle_size: @device.middle_size, 
+													short_size: @device.short_size, 
+													tags: params[:tags] 
+											}).page(params[:page]).per(3).order(:id)
 	end
 
 	# tag
-	# page
-	# device
+	# = form_for @search_form, url: hoge_path, remote: true, html: {method: :get} do |f|
+	# = f.check_box :foobar, {multiple: true}, 'foo', nil
+	# = f.check_box :foobar, {multiple: true}, 'bar', nil
+
+	# gem 'kaminari'
+	# <%= paginate(@bags) %>
+
+	# strong_parameter
 
 end
