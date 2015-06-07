19e1bca15b00edacf4389d1979234527615a672b
Sho Yokoyama <sho.y0921@gmail.com>
2015-06-07 22:59:50 +0900

    try to draw goal by html and devide select-boxes into some pages

--- app/controllers/device_brands_controller.rb
+++ app/controllers/device_brands_controller.rb
@@ -2,6 +2,7 @@ class DeviceBrandsController < ApplicationController
 
 	def index
 		@device_brands = DeviceBrand.where('tree_depth = ?', 0).includes(:device_items)
+		@device_brand = DeviceBrand.new(name: 'hoge')
 	end
 
 end
