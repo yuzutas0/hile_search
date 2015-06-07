42e23d9e58118bfb8f9166cbe18bda405ee51352
Sho Yokoyama <sho.y0921@gmail.com>
2015-06-07 23:00:34 +0900

    fix bug

--- app/controllers/device_brands_controller.rb
+++ app/controllers/device_brands_controller.rb
@@ -2,7 +2,6 @@ class DeviceBrandsController < ApplicationController
 
 	def index
 		@device_brands = DeviceBrand.where('tree_depth = ?', 0).includes(:device_items)
-		@device_brand = DeviceBrand.new(name: 'hoge')
 	end
 
 end
