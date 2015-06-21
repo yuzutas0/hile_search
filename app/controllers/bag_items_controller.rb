70cef412d7e603c59bc067ed5056331513e676a5
yuzutas0 <sho.y0921@gmail.com>
2015-06-21 20:48:12 +0900

    make search-by-bag-tags in bag-items-page

--- app/controllers/bag_items_controller.rb
+++ app/controllers/bag_items_controller.rb
@@ -6,12 +6,21 @@ class BagItemsController < ApplicationController
 		max_dii = DeviceItem.maximum(:id)
 		if dii.present? && integer_string?(dii) && dii.to_i <= max_dii
 
+			# check params sbt
+			@sbt = params[:sbt]
+			if @sbt.blank? || !(sbt_is_right?(@sbt))
+				@sbt = BagTag.pluck(:id)
+			end
+
 			# get bags by device
 			@device = DeviceItem.find(dii)
-			@bags = BagItem.where("long_side > :long_side AND middle_side > :middle_side AND (short_side = 0 OR short_side > :short_side)",{
+			bag_id_by_tags = BagItem.joins(:bag_tags).where("bag_tag_id IN (:tags)", tags: @sbt).pluck(:id)
+			@bags = BagItem.where("long_side > :long_side AND middle_side > :middle_side AND \
+												(short_side = 0 OR short_side > :short_side) AND id IN (:id_by_tags)", {
 																long_side: @device.long_side, 
 																middle_side: @device.middle_side, 
 																short_side: @device.short_side, 
+																id_by_tags: bag_id_by_tags
 														}).page(params[:page]).per(3).order(:id).includes({:bag_tags => :parent})
 			if @device.present? && @bags.present?
 
@@ -21,6 +30,7 @@ class BagItemsController < ApplicationController
 				end
 
 				# view
+				@tags = BagTag.where('tree_depth = ?', 0).includes(:children)
 				return
 			end
 		end
@@ -48,6 +58,14 @@ class BagItemsController < ApplicationController
 			true
 		rescue ArgumentError
 			false
+		end
+
+		def sbt_is_right?(array)
+			max_sbt = BagTag.maximum(:id)
+			for sbt_item in array
+				return false if !(integer_string?(sbt_item)) || sbt_item.to_i > max_sbt
+			end
+			return true
 		end
 
 end
